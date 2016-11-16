f85c2dee685fba20a54088d8e440ff677bd40abf
jessehartloff <hartloff@buffalo.edu>
2016-11-16 01:31:48 -0500

    adding mongodb

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,20 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/listQuizzes', function (req, res, next) {
-    res.end("TODO"); // TODO
+router.get('//listQuizzes', function (req, res, next) {
+
+    var db = req.db;
+    var collection = db.get('quizapi');
+    collection.find({}, {}, function (e, docs) {
+        //res.render('stuff', {
+        //    "stuff": docs
+        //});
+        res.end(docs);
+    });
 });
 
 
-router.get('/getQuiz', function (req, res, next) {
+router.get('//getQuiz', function (req, res, next) {
     if (req.query.hasOwnProperty('quiz')) {
         res.end("{" + req.query.quiz + "}");
     } else {
@@ -22,28 +30,28 @@ router.get('/getQuiz', function (req, res, next) {
 });
 
 
-router.post('/addQuiz', function (req, res, next) {
+router.post('//addQuiz', function (req, res, next) {
     res.end("TODO"); // TODO
 });
 
-router.post('/addQuestion', function (req, res, next) {
+router.post('//addQuestion', function (req, res, next) {
     res.end("TODO"); // TODO
 });
 
-router.post('/addKeyword', function (req, res, next) {
+router.post('//addKeyword', function (req, res, next) {
     res.end("TODO"); // TODO
 });
 
 
-router.get('/addQuiz', function (req, res, next) {
+router.get('//addQuiz', function (req, res, next) {
     res.end("Use POST to add a quiz");
 });
 
-router.get('/addQuestion', function (req, res, next) {
+router.get('//addQuestion', function (req, res, next) {
     res.end("Use POST to add a question");
 });
 
-router.get('/addKeyword', function (req, res, next) {
+router.get('//addKeyword', function (req, res, next) {
     res.end("Use POST to add a keyword");
 });
 
