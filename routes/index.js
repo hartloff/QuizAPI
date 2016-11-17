d150b3e553d46460a997e958dba3f1251ebc6e73
jessehartloff <hartloff@buffalo.edu>
2016-11-17 11:48:20 -0500

    add POST functionality

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ router.get('/', function (req, res, next) {
 router.get('//listQuizzes', function (req, res, next) {
     var db = req.db;
     var collection = db.get('quizapi');
-    collection.find({}, {"quiz_name": 1, "_id":0,"questions":0}, function (e, docs) {
+    collection.find({}, {"quiz_name": 1, "_id": 0, "questions": 0}, function (e, docs) {
         var quizzes = [];
-        for(var i in docs){
-            quizzes.add(docs[i].quiz_name);
+        for (var i in docs) {
+            quizzes.push(docs[i].quiz_name);
         }
         res.send(quizzes);
     });
@@ -28,7 +28,7 @@ router.get('//getQuiz', function (req, res, next) {
     if (req.query.hasOwnProperty('quiz')) {
         var db = req.db;
         var collection = db.get('quizapi');
-        collection.find({"quiz_name": req.query.quiz}, {}, function (e, docs) {
+        collection.find({"quiz_name": req.query.quiz}, {"_id": 0}, function (e, docs) {
             res.send(docs);
         });
     } else {
@@ -40,11 +40,34 @@ router.get('//getQuiz', function (req, res, next) {
 // POST requests
 
 router.post('//addQuiz', function (req, res, next) {
-    res.end("TODO"); // TODO
+    if (req.body.hasOwnProperty("quiz_name")) {
+        var quiz_name = req.body.quiz_name;
+        var db = req.db;
+        var collection = db.get('quizapi');
+        // TODO: Check if quiz exists
+        collection.insert({
+            "quiz_name": quiz_name,
+            "questions": []
+        });
+    } else {
+        res.end("Invalid Request: Send a JSON string with a variable called 'quiz_name' containing the name of your " +
+            "quiz. ex: {'quiz_name':'CSE191_midterm'}");
+    }
 });
 
 router.post('//addQuestion', function (req, res, next) {
-    res.end("TODO"); // TODO
+    if (req.body.hasOwnProperty("question") && req.body.hasOwnProperty("quiz")) {
+        var question = req.body.question;
+        var quiz_name = req.body.quiz;
+        var db = req.db;
+        var collection = db.get('quizapi');
+        collection.update({"quiz_name": quiz_name}, {$push: {"questions": question}});
+        // TODO: Check if quiz exists
+        // TODO: Check if question is properly formatted? Might leave this up to an exception handling lesson when parsing quizzes
+    } else {
+        res.end("Invalid Request: Send a JSON string with a variable called 'question' containing all the details" +
+            " of the question to add and a variable called 'quiz' containing the name of the quiz to modify");
+    }
 });
 
 router.post('//addKeyword', function (req, res, next) {
