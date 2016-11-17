ee5517f13cea8e704acc9e6cf58c231dc87d1f01
jessehartloff <hartloff@buffalo.edu>
2016-11-16 23:26:05 -0500

    add GET API functions

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 
+// home page
+
 router.get('/', function (req, res, next) {
     res.end("/listQuizzes\n/getQuiz\n/addQuiz\n/addQuestion\n/addKeyword");
 });
 
 
-router.get('//listQuizzes', function (req, res, next) {
+// GET requests
 
+router.get('//listQuizzes', function (req, res, next) {
     var db = req.db;
     var collection = db.get('quizapi');
-    collection.find({}, {}, function (e, docs) {
-        //res.render('stuff', {
-        //    "stuff": docs
-        //});
-        res.end(docs);
+    collection.find({}, {"quiz_name": 1}, function (e, docs) {
+        res.send(docs);
     });
 });
 
 
 router.get('//getQuiz', function (req, res, next) {
     if (req.query.hasOwnProperty('quiz')) {
-        res.end("{" + req.query.quiz + "}");
+        var db = req.db;
+        var collection = db.get('quizapi');
+        collection.find({"quiz_name": req.query.quiz}, {}, function (e, docs) {
+            res.send(docs);
+        });
     } else {
-        res.end("You done bad");
+        res.end("Invalid Request: url must be in the form /getQuiz?quiz=quizName. To get a list of valid quizNames use /listQuizzes");
     }
-    res.end("TODO"); // TODO
 });
 
+
+// POST requests
 
 router.post('//addQuiz', function (req, res, next) {
     res.end("TODO"); // TODO
@@ -42,6 +47,8 @@ router.post('//addKeyword', function (req, res, next) {
     res.end("TODO"); // TODO
 });
 
+
+// Not used
 
 router.get('//addQuiz', function (req, res, next) {
     res.end("Use POST to add a quiz");
